23131066d4bd3dcb536aeb3ab17d1d3eb87a9d14
pedroobando <pedroobando@hotmail.com>
2021-01-19 16:45:25 -0400

    install service

--- src/services.js
+++ src/services.js
@@ -1,17 +1,23 @@
-var Service = require('node-windows').Service;
+const Service = require('node-windows').Service;
+const ruta = require('path');
 // const dotenv = require('dotenv');
 // dotenv.config();
+console.log(ruta.join(__dirname, 'index.js'));
 
+const { readDataConfig } = require('./helperfunc');
+
+const valoresConfigJson = readDataConfig('config.json');
 // Create a new service object
 var svc = new Service({
-  name: 'Balanza Lector',
+  name: 'Balanza Comunicacion',
   description:
     'Servidor web en nodejs, lee el puerto com? de balanza, enviando el resuntado por http:',
-  script: 'C:\\Users\\pedro\\node\\apiserialport\\src\\index.js',
+  // script: 'C:\\Users\\pedro\\node\\apiserialport\\src\\index.js',
+  script: ruta.join(__dirname, 'index.js'),
   env: {
-    PORT: 4000,
-    BALANZAPORTCOM: 'COM4',
-    BALANZABAUDIOS: 9600,
+    PORT: valoresConfigJson.PORTHTTP,
+    BALANZAPORTCOM: valoresConfigJson.BALANZAPORTCOM,
+    BALANZABAUDIOS: valoresConfigJson.BALANZABAUDIOS,
   },
   nodeOptions: ['--harmony', '--max_old_space_size=4096'],
   //, workingDirectory: '...'
